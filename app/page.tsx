689089a255148b6b76c3a46d0634d91aefa5842a
BenediktKd <benedikt@uc.cl>
2023-09-24 01:42:54 -0300

    new question

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,23 @@ import React, { useState } from 'react';
 import './globals.css';
 
 const HomePage: React.FC = () => {
+  // Estado para mantener la conexión WebSocket
   const [ws, setWs] = useState<WebSocket | null>(null);
 
-  // Función para manejar el evento de unirse a la trivia
+  // Función que se ejecuta cuando el usuario intenta unirse a la trivia
   const handleJoinTrivia = () => {
+    // Obtiene el número del estudiante y el nombre de usuario de los campos de entrada
     const studentNumber = (document.getElementById('studentNumber') as HTMLInputElement).value;
     const username = (document.getElementById('username') as HTMLInputElement).value || 'DefaultUsername';
 
-    // Establecer la conexión WebSocket
+    // Establece una nueva conexión WebSocket
     const websocket = new WebSocket('wss://trivia.tallerdeintegracion.cl/connect');
 
+    // Función que se ejecuta cuando la conexión WebSocket se abre correctamente
     websocket.onopen = () => {
       console.log("WebSocket connection opened");
 
-      // Enviar el evento JOIN una vez que se establezca la conexión
+      // Envía un mensaje de tipo 'join' para unirse a la trivia
       websocket.send(JSON.stringify({
         type: 'join',
         id: studentNumber,
@@ -25,18 +28,23 @@ const HomePage: React.FC = () => {
       }));
     };
 
+    // Función que se ejecuta cuando se recibe un mensaje a través de la conexión WebSocket
     websocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log(data);
 
+      // Si el mensaje es de tipo 'accepted', guarda un indicador en el localStorage
       if (data.type === 'accepted') {
         localStorage.setItem('shouldContinueListening', 'true');
-      } else if (data.type === 'question') {
+      } 
+      // Si el mensaje es de tipo 'question', muestra una alerta y redirige a la página de pregunta
+      else if (data.type === 'question') {
         alert("Te has unido a una partida en curso. Comenzarás con 0 puntos.");
         window.location.href = '/question';
       }
     };
 
+    // Actualiza el estado con la conexión WebSocket
     setWs(websocket);
   };
 
@@ -45,6 +53,7 @@ const HomePage: React.FC = () => {
       <h2 className="text-3xl font-bold mb-4">¡Bienvenido a Mystical Trivia Quest!</h2>
       <p className="text-lg mb-8">Únete a la aventura y demuestra tus conocimientos.</p>
 
+      {/* // Campo de entrada para el número del estudiante */}
       <div className="mb-4">
         <label htmlFor="studentNumber" className="block text-sm font-medium text-gray-700">Número de alumno</label>
         <input 
@@ -54,6 +63,7 @@ const HomePage: React.FC = () => {
         />
       </div>
 
+      {/* // Campo de entrada para el nombre de usuario */}
       <div className="mb-4">
         <label htmlFor="username" className="block text-sm font-medium text-gray-700">Usuario (opcional)</label>
         <input 
@@ -63,6 +73,7 @@ const HomePage: React.FC = () => {
         />
       </div>
 
+      {/* // Botón para unirse a la trivia */}
       <button onClick={handleJoinTrivia} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Unirse a la Trivia
       </button>
@@ -71,4 +82,5 @@ const HomePage: React.FC = () => {
 }
 
 export default HomePage;
+
 
--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,44 +1,104 @@
+// Importamos las dependencias necesarias
 'use client'
-
 import React, { useEffect, useState } from 'react';
 import '../globals.css';
 
+// Definimos la interfaz para la estructura de datos de la pregunta
+interface Question {
+  trivia_id: string;
+  type: string;
+  question: {
+    id: number;
+    type: string;
+    title: string;
+    points: number;
+    options?: { [key: number]: string };
+  };
+}
+
 const QuestionPage: React.FC = () => {
-  const [ws, setWs] = useState<WebSocket | null>(null);
+  // Usamos el hook useState para mantener la información de la pregunta en el estado local
+  const [questionData, setQuestionData] = useState<Question | null>(null);
 
+  // Usamos el hook useEffect para ejecutar código cuando el componente se monta
   useEffect(() => {
-    if (localStorage.getItem('shouldContinueListening') === 'true') {
-      const websocket = new WebSocket('wss://trivia.tallerdeintegracion.cl/connect');
-      
-      websocket.onopen = () => {
-        console.log("WebSocket connection opened in QuestionPage");
-      };
+    // Asumimos que hemos guardado la conexión WebSocket en una variable global
+    const ws: WebSocket = (window as any).websocket; 
 
-      websocket.onmessage = (event) => {
+    if (ws) {
+      // Establecemos un manejador de eventos para cuando se recibe un mensaje a través del WebSocket
+      ws.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        console.log(data);
-        // Aquí manejas los mensajes que recibes en la página de pregunta
+        // Si el tipo de mensaje es "question", actualizamos el estado con la información de la pregunta
+        if (data.type === "question") {
+          setQuestionData(data);
+        }
       };
-
-      setWs(websocket);
     }
-
-    return () => {
-      localStorage.removeItem('shouldContinueListening');
-      if (ws) {
-        ws.close();
-      }
-    };
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-3xl font-bold mb-4">Pregunta</h2>
-      {/* Aquí iría el resto de tu diseño para mostrar la pregunta y las opciones de respuesta */}
+      {/* Si tenemos datos de la pregunta, los mostramos */}
+      {questionData && (
+        <>
+          {/* Mostramos el título de la pregunta */}
+          <h2 className="text-3xl font-bold mb-4">{questionData.question.title}</h2>
+          {/* Mostramos los puntos de la pregunta */}
+          <p className="text-lg mb-8">Puntos: {questionData.question.points}</p>
+
+          {/* Si el tipo de pregunta es "button", mostramos las opciones como botones */}
+          {questionData.question.type === 'button' && questionData.question.options && (
+            <div className="mb-4">
+              {Object.entries(questionData.question.options).map(([key, value]) => (
+                <button key={key} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
+                  {value}
+                </button>
+              ))}
+            </div>
+          )}
+
+          {/* Si el tipo de pregunta es "text", mostramos un campo de entrada para texto */}
+          {questionData.question.type === 'text' && (
+            <div className="mb-4">
+              <input 
+                type="text" 
+                placeholder="Escribe tu respuesta aquí" 
+                className="mt-1 p-2 w-64 border rounded-md text-black"
+              />
+            </div>
+          )}
+
+          {/* Si el tipo de pregunta es "chat", mostramos un área de texto */}
+          {questionData.question.type === 'chat' && (
+            <div className="mb-4">
+              <textarea 
+                placeholder="Escribe tu respuesta aquí" 
+                className="mt-1 p-2 w-64 h-32 border rounded-md text-black"
+              />
+            </div>
+          )}
+
+          {/* Mostramos un botón para enviar la respuesta */}
+          <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Enviar Respuesta
+          </button>
+        </>
+      )}
     </div>
   );
 }
 
 export default QuestionPage;
+
+
+
+
+// return (
+//     <div className="flex flex-col items-center justify-center min-h-screen">
+//       <h2 className="text-3xl font-bold mb-4">Pregunta</h2>
+//       {/* Aquí iría el resto de tu diseño para mostrar la pregunta y las opciones de respuesta */}
+//     </div>
+//   );
 
 

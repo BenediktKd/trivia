ad6268b914c7f0eb8b9f0a942bd94ce9be908f6f
BenediktKd <benedikt@uc.cl>
2023-09-23 21:30:00 -0300

    fix[landing_page]: add Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+"use client"
+
+import React from 'react';
+import Link from 'next/link';
 import './globals.css';
 
 const HomePage: React.FC = () => {
-  const router = useRouter();
-
-  // Estados para los valores de los inputs
-  const [studentNumber, setStudentNumber] = useState('');
-  const [username, setUsername] = useState('');
-
-  const handleJoinClick = () => {
-    // Aquí puedes manejar la validación o enviar los datos antes de redirigir
-    router.push('/lobby');  // Redirige al usuario a la página del lobby
-  }
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-3xl font-bold mb-4">¡Bienvenido a Mystical Trivia Quest!</h2>
@@ -24,8 +15,6 @@ const HomePage: React.FC = () => {
         <input 
           type="text" 
           id="studentNumber" 
-          value={studentNumber} 
-          onChange={(e) => setStudentNumber(e.target.value)} 
           className="mt-1 p-2 w-64 border rounded-md"
         />
       </div>
@@ -35,15 +24,15 @@ const HomePage: React.FC = () => {
         <input 
           type="text" 
           id="username" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
           className="mt-1 p-2 w-64 border rounded-md"
         />
       </div>
 
-      <button onClick={handleJoinClick} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Unirse a la Trivia
-      </button>
+      <Link href="/lobby">
+        <a className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Unirse a la Trivia
+        </a>
+      </Link>
     </div>
   );
 }

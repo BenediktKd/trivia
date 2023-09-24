19affe33f56122cc5f598409eac13d49141e6bb3
BenediktKd <benedikt@uc.cl>
2023-09-24 01:46:22 -0300

    new question

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,9 +1,7 @@
-// Importamos las dependencias necesarias
 'use client'
 import React, { useEffect, useState } from 'react';
 import '../globals.css';
 
-// Definimos la interfaz para la estructura de datos de la pregunta
 interface Question {
   trivia_id: string;
   type: string;
@@ -17,37 +15,37 @@ interface Question {
 }
 
 const QuestionPage: React.FC = () => {
-  // Usamos el hook useState para mantener la información de la pregunta en el estado local
   const [questionData, setQuestionData] = useState<Question | null>(null);
 
-  // Usamos el hook useEffect para ejecutar código cuando el componente se monta
   useEffect(() => {
-    // Asumimos que hemos guardado la conexión WebSocket en una variable global
-    const ws: WebSocket = (window as any).websocket; 
+    console.log("Componente QuestionPage montado."); // Verificar que el componente se monta
+
+    const ws: WebSocket = (window as any).websocket;
 
     if (ws) {
-      // Establecemos un manejador de eventos para cuando se recibe un mensaje a través del WebSocket
+      console.log("WebSocket encontrado."); // Verificar que el WebSocket existe
+
       ws.onmessage = (event) => {
+        console.log("Mensaje recibido:", event.data); // Imprimir cada mensaje que se recibe
+
         const data = JSON.parse(event.data);
-        // Si el tipo de mensaje es "question", actualizamos el estado con la información de la pregunta
         if (data.type === "question") {
+          console.log("Datos de la pregunta:", data); // Imprimir los datos de la pregunta
           setQuestionData(data);
         }
       };
+    } else {
+      console.log("WebSocket no encontrado."); // Si no se encuentra el WebSocket
     }
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      {/* Si tenemos datos de la pregunta, los mostramos */}
       {questionData && (
         <>
-          {/* Mostramos el título de la pregunta */}
           <h2 className="text-3xl font-bold mb-4">{questionData.question.title}</h2>
-          {/* Mostramos los puntos de la pregunta */}
           <p className="text-lg mb-8">Puntos: {questionData.question.points}</p>
 
-          {/* Si el tipo de pregunta es "button", mostramos las opciones como botones */}
           {questionData.question.type === 'button' && questionData.question.options && (
             <div className="mb-4">
               {Object.entries(questionData.question.options).map(([key, value]) => (
@@ -58,7 +56,6 @@ const QuestionPage: React.FC = () => {
             </div>
           )}
 
-          {/* Si el tipo de pregunta es "text", mostramos un campo de entrada para texto */}
           {questionData.question.type === 'text' && (
             <div className="mb-4">
               <input 
@@ -69,7 +66,6 @@ const QuestionPage: React.FC = () => {
             </div>
           )}
 
-          {/* Si el tipo de pregunta es "chat", mostramos un área de texto */}
           {questionData.question.type === 'chat' && (
             <div className="mb-4">
               <textarea 
@@ -79,7 +75,6 @@ const QuestionPage: React.FC = () => {
             </div>
           )}
 
-          {/* Mostramos un botón para enviar la respuesta */}
           <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Enviar Respuesta
           </button>
@@ -94,11 +89,6 @@ export default QuestionPage;
 
 
 
-// return (
-//     <div className="flex flex-col items-center justify-center min-h-screen">
-//       <h2 className="text-3xl font-bold mb-4">Pregunta</h2>
-//       {/* Aquí iría el resto de tu diseño para mostrar la pregunta y las opciones de respuesta */}
-//     </div>
-//   );
+
 
 

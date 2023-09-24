b6bf7f654ee974297f998cdd82e2d5c596ec7f5c
BenediktKd <benedikt@uc.cl>
2023-09-24 01:55:36 -0300

    new question

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import './globals.css';
 
 const HomePage: React.FC = () => {
-  // Estado para mantener la conexión WebSocket
-  const [ws, setWs] = useState<WebSocket | null>(null);
 
-  // Función que se ejecuta cuando el usuario intenta unirse a la trivia
   const handleJoinTrivia = () => {
-    // Obtiene el número del estudiante y el nombre de usuario de los campos de entrada
     const studentNumber = (document.getElementById('studentNumber') as HTMLInputElement).value;
     const username = (document.getElementById('username') as HTMLInputElement).value || 'DefaultUsername';
 
-    // Establece una nueva conexión WebSocket
+    // Establecer la conexión WebSocket
     const websocket = new WebSocket('wss://trivia.tallerdeintegracion.cl/connect');
 
-    // Función que se ejecuta cuando la conexión WebSocket se abre correctamente
+    // Almacenar el objeto WebSocket en el objeto window
+    (window as any).websocket = websocket;
+
     websocket.onopen = () => {
       console.log("WebSocket connection opened");
-
-      // Envía un mensaje de tipo 'join' para unirse a la trivia
       websocket.send(JSON.stringify({
         type: 'join',
         id: studentNumber,
@@ -28,24 +24,17 @@ const HomePage: React.FC = () => {
       }));
     };
 
-    // Función que se ejecuta cuando se recibe un mensaje a través de la conexión WebSocket
     websocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log(data);
 
-      // Si el mensaje es de tipo 'accepted', guarda un indicador en el localStorage
       if (data.type === 'accepted') {
-        localStorage.setItem('shouldContinueListening', 'true');
-      } 
-      // Si el mensaje es de tipo 'question', muestra una alerta y redirige a la página de pregunta
-      else if (data.type === 'question') {
+        console.log("Accepted event received.");
+      } else if (data.type === 'question') {
         alert("Te has unido a una partida en curso. Comenzarás con 0 puntos.");
         window.location.href = '/question';
       }
     };
-
-    // Actualiza el estado con la conexión WebSocket
-    setWs(websocket);
   };
 
   return (
@@ -53,7 +42,6 @@ const HomePage: React.FC = () => {
       <h2 className="text-3xl font-bold mb-4">¡Bienvenido a Mystical Trivia Quest!</h2>
       <p className="text-lg mb-8">Únete a la aventura y demuestra tus conocimientos.</p>
 
-      {/* // Campo de entrada para el número del estudiante */}
       <div className="mb-4">
         <label htmlFor="studentNumber" className="block text-sm font-medium text-gray-700">Número de alumno</label>
         <input 
@@ -63,7 +51,6 @@ const HomePage: React.FC = () => {
         />
       </div>
 
-      {/* // Campo de entrada para el nombre de usuario */}
       <div className="mb-4">
         <label htmlFor="username" className="block text-sm font-medium text-gray-700">Usuario (opcional)</label>
         <input 
@@ -73,7 +60,6 @@ const HomePage: React.FC = () => {
         />
       </div>
 
-      {/* // Botón para unirse a la trivia */}
       <button onClick={handleJoinTrivia} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Unirse a la Trivia
       </button>
@@ -82,5 +68,6 @@ const HomePage: React.FC = () => {
 }
 
 export default HomePage;
+
 
 
--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,90 +1,34 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+
+import React, { useEffect } from 'react';
 import '../globals.css';
 
-interface Question {
-  trivia_id: string;
-  type: string;
-  question: {
-    id: number;
-    type: string;
-    title: string;
-    points: number;
-    options?: { [key: number]: string };
-  };
-}
-
 const QuestionPage: React.FC = () => {
-  const [questionData, setQuestionData] = useState<Question | null>(null);
-
   useEffect(() => {
-    console.log("Componente QuestionPage montado."); // Verificar que el componente se monta
+    const websocket = (window as any).websocket;
 
-    const ws: WebSocket = (window as any).websocket;
-
-    if (ws) {
-      console.log("WebSocket encontrado."); // Verificar que el WebSocket existe
-
-      ws.onmessage = (event) => {
-        console.log("Mensaje recibido:", event.data); // Imprimir cada mensaje que se recibe
-
+    if (websocket) {
+      websocket.onmessage = (event: MessageEvent) => { // Aquí especificamos el tipo
         const data = JSON.parse(event.data);
-        if (data.type === "question") {
-          console.log("Datos de la pregunta:", data); // Imprimir los datos de la pregunta
-          setQuestionData(data);
-        }
+        console.log("Data received on QuestionPage:", data);
+        // Aquí manejas los mensajes que recibes en la página de pregunta
       };
     } else {
-      console.log("WebSocket no encontrado."); // Si no se encuentra el WebSocket
+      console.error("WebSocket not found on QuestionPage.");
     }
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      {questionData && (
-        <>
-          <h2 className="text-3xl font-bold mb-4">{questionData.question.title}</h2>
-          <p className="text-lg mb-8">Puntos: {questionData.question.points}</p>
-
-          {questionData.question.type === 'button' && questionData.question.options && (
-            <div className="mb-4">
-              {Object.entries(questionData.question.options).map(([key, value]) => (
-                <button key={key} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
-                  {value}
-                </button>
-              ))}
-            </div>
-          )}
-
-          {questionData.question.type === 'text' && (
-            <div className="mb-4">
-              <input 
-                type="text" 
-                placeholder="Escribe tu respuesta aquí" 
-                className="mt-1 p-2 w-64 border rounded-md text-black"
-              />
-            </div>
-          )}
-
-          {questionData.question.type === 'chat' && (
-            <div className="mb-4">
-              <textarea 
-                placeholder="Escribe tu respuesta aquí" 
-                className="mt-1 p-2 w-64 h-32 border rounded-md text-black"
-              />
-            </div>
-          )}
-
-          <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Enviar Respuesta
-          </button>
-        </>
-      )}
+      <h2 className="text-3xl font-bold mb-4">Pregunta</h2>
+      {/* Aquí puedes renderizar la pregunta y las opciones basado en los datos recibidos */}
     </div>
   );
 }
 
 export default QuestionPage;
+
+
 
 
 

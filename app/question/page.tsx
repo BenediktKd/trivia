1ac467fb07201a1c5771eeb3165e38f4f40e18ba
BenediktKd <benedikt@uc.cl>
2023-09-24 01:12:17 -0300

    fix[lobby]: test

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import './globals.css';
+import '../globals.css';
 
 const QuestionPage: React.FC = () => {
   const [ws, setWs] = useState<WebSocket | null>(null);
